896851e03bc60b19ff5961a8d0c2d4a0973079d8
OscarRangelM <orangelm1300@alumno.ipn.mx>
2023-03-01 01:47:22 -0600

    Styles form page

--- client/src/components/FormPage/Form.jsx
+++ client/src/components/FormPage/Form.jsx
@@ -10,15 +10,6 @@ FORM PAGE |: en esta vista se encontrará el formulario para crear un nuevo poke
 
 Este formulario debe ser controlado completamente con JavaScritp. No se pueden utilizar validaciones HTML, ni utilizar librerías especiales para esto. Debe contar con los siguientes campos:
 
-Nombre.
-Imagen.
-Vida.
-Ataque.
-Defensa.
-Velocidad (si tiene).
-Altura (si tiene).
-Peso (si tiene).
-Posibilidad de seleccionar/agregar varios tipos en simultáneo.
 Botón para crear el nuevo pokemon.
 [IMPORANTE]: es requisito que el formulario de creación esté validado sólo con JavaScript. 
 Puedes agregar las validaciones que consideres. Por ejemplo: que el nombre del pokemon no pueda contener números, o que la defensa no pueda exceder determinado valor, etc. 
@@ -35,31 +26,43 @@ export default function Form(props) {
         <div className={styles.divForm} >
             <nav className={styles.nav} >
                 <NavLink to="../home" >
-                    <button>Back Home</button>
+                    <button className={styles.bttnBack}>Back Home</button>
                 </NavLink>
-                <h1>Form page</h1>
+                <h1>Create a New Pokemon</h1>
             </nav>
             <form className={styles.form} >
                 <label htmlFor="inputName">Name</label>
-                <input type='text' placeholder='Name' />
+                <input type='text' placeholder='Name' className={styles.inputName}/>
+
                 <label htmlFor="inputImage">Image</label>
                 <input type='text' placeholder='URL of image' />
+
                 <label htmlFor="inputHP">HP:</label>
                 <input type="number" placeholder='HP' />
+
                 <label htmlFor="inputAttack">Attack</label>
                 <input type='number' placeholder='Attack' />
+
                 <label htmlFor="inputDefense">Defense</label>
                 <input type='number' placeholder='Defense' />
+
                 <label htmlFor="inputSpeed">Speed</label>
                 <input type='number' placeholder='Speed' />
+
                 <label htmlFor="inputHeight">Height</label>
                 <input type='number' placeholder='Height' />
+
                 <label htmlFor="inputWeight">Weight</label>
                 <input type='number' placeholder='Weight' />
+
                 <label htmlFor="inputTypes">Types</label>
-                <input type='select' placeholder='Types-1' ></input>
-                <input type='select' placeholder='Types-2' ></input>
-                <input type='submit' />
+                <select type='select' placeholder='Types-1' >
+                    <option>None</option>
+                </select>
+                <select type='select' placeholder='Types-2' >
+                    <option>None</option>
+                </select>
+                <input type='submit' className={styles.bttnSubmit} value='Submit' />
             </form>
         </div>
     );
