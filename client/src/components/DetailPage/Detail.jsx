6b0a29b77a72312fc942b9d1ad0bdbeb27ec3888
OscarRangelM <orangelm1300@alumno.ipn.mx>
2023-02-28 21:33:02 -0600

    Details complete

--- client/src/components/DetailPage/Detail.jsx
+++ client/src/components/DetailPage/Detail.jsx
@@ -1,28 +1,16 @@
 import styles from './detail.module.css';
-import React from 'react';
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { useParams, Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from 'react-router-dom';
 import { findPokemon } from '../../redux/actions/index.js'
 
 /* 
 DETAIL PAGE | en esta vista se deberá mostrar toda la información específica de un pokemon:
 
-ID.
-Nombre.
-Imagen.
-Vida.
-Ataque.
-Defensa.
-Velocidad (si tiene).
-Altura (si tiene).
-Peso (si tiene).
 Tipo. */
 export default function Detail() {
 
     const { detailId } = useParams();
-    // console.log(`id: ${detailId}`);
     const dispatch = useDispatch();
     const state = useSelector(state => state.pokemonDetail);
 
@@ -30,17 +18,22 @@ export default function Detail() {
         dispatch(findPokemon(detailId));
     }, [dispatch, detailId]);
 
-    // console.log(`State ${state}`);
-    // console.log(`State ${Object.keys(state).length}`);
+    // const {name} = state.type
+    const arrType = (typeof(state.Types) === 'undefined') ? state.type : state.Types
+    // console.log(`State ${state.Types}`);
+    console.log(arrType)
 
     return (
         <div className={styles.divDetail} >
-            <Link to='../home' >
-                <button>Back</button>
-            </Link>
-            <h1>{state.name}</h1>
-            <img src={state.image} alt={state.name} />
-            <section>
+            <section className={styles.detailHeader}>
+                <Link to='../home' >
+                    <button className={styles.bttnBack}>Back</button>
+                </Link>
+                <h1 className={styles.detailHeaderName}>{state.name}</h1>
+            </section>
+            <section className={styles.detailBody}>
+                <img src={state.image} alt={state.name} />
+                <div>
                 <h3>ID: {state.id}</h3>
                 <h3>HP: {state.hp}</h3>
                 <h3>Attack: {state.attack}</h3>
@@ -48,13 +41,13 @@ export default function Detail() {
                 <h3>Speed: {state.speed}</h3>
                 <h3>Height: {state.height}</h3>
                 <h3>Weight: {state.weight}</h3>
-                <div className= {styles.pokeTypes}><h3>Type(s):</h3> 
-                    {/* {state.type.map(result => {
-                        console.log(result);
-                        return (<p className={styles.stylesType} key={result.TypePokemon.TypeId
-                        }>{result.name}</p>);
-                    })} */}
-            </div >
+                <div className={styles.pokeTypes}><h3>Type(s): </h3>
+                    {arrType?.map(result => {
+                        console.log(result.name);
+                        return (<p className={styles.stylesType} >{result.name}</p>);
+                    })}
+                </div >
+                </div>
             </section>
         </div>
     );
